2ea632e3ecd16959b8cac44d4f321713478379b8
Interparvus <michal.holan.on@gmail.com>
2022-04-25 19:53:03 +0200

    errors encounters

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from copy import copy
 from datetime import date
+from shutil import copyfile
+
+
+fileBackupDate = date.today()
+fileBackup = str(fileBackupDate).replace("-",",")
+
+input = r"C:/Users/admin/OneDrive - Gymnazium/Pracovnáplocha/folderBackup/folderOriginal"
+output = r"C:/Users/admin/OneDrive - Gymnazium/Pracovná plocha/folderBackup/folderOriginalBackup" + fileBackup 
+copyfile(input,output)
