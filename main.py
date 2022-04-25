d7f973b3268cd1923d3ef24d95750805963babe4
Interparvus <michal.holan.on@gmail.com>
2022-04-25 20:13:22 +0200

    ErrorsFixed

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from copy import copy
+
 from datetime import date
-from shutil import copyfile
+from distutils.dir_util import copy_tree
 
 
 fileBackupDate = date.today()
 fileBackup = str(fileBackupDate).replace("-",",")
 
-input = r"C:/Users/admin/OneDrive - Gymnazium/Pracovnáplocha/folderBackup/folderOriginal"
+input = r"C:/Users/admin/OneDrive - Gymnazium/Pracovná plocha/folderBackup/folderOriginal"
 output = r"C:/Users/admin/OneDrive - Gymnazium/Pracovná plocha/folderBackup/folderOriginalBackup" + fileBackup 
-copyfile(input,output)
+
+
+copy_tree(input,output)
